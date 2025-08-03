bcebc718bd1c0d7062787429b93b884da7471f6c
olshanchenko.dmitriy <olshanchenko.dmitriy@gmail.com>
2025-08-03 14:59:22 +0000

    Initial commit

---
+++ android/app/src/main/java/com/scribe/com/MainActivity.java
@@ -0,0 +1,5 @@
+package com.scribe.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.scribe.com',
+  appName: 'scribe ',
+  webDir: 'dist'
+};
+
+export default config;
